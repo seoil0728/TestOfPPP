f51dd59b5a3f53e75a5f067b11bab1ca154b5139
seoil0728 <swoo0728@naver.com>
2023-05-22 15:47:08 +0900

    EungAeBackup

--- Assets/02_Scripts_KSW/GameManager.cs
+++ Assets/02_Scripts_KSW/GameManager.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Photon.Pun;
 
-public class GameManager : MonoBehaviour
+public enum GameState
+{
+    idle, ready, roulette, play, finished
+}
+public class GameManager : MonoBehaviourPun
 {
     public Transform iceGround;
+    public Transform giantPos;
+    public Transform miniPos;
+    public Material deadMat;
+    Transform deadZone;
+    GameState gameState;
+
+    bool playingSettings = false;
+    int round = 0;
+    int deadZoneCount = 1;
+    
+
+
     void Start()
     {
+        gameState = GameState.idle;
         Transform[] ices = iceGround.GetComponentsInChildren<Transform>();
         foreach (Transform t in ices)
         {
@@ -15,9 +33,108 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        GameFlow();
+        GameRule();
+    }
+
+    void GameFlow()
+    {
+        switch(gameState)
+        {
+            case GameState.idle:
+                // idle 상태에서 플레이어 접속을 체크
+
+                gameState = GameState.ready;
+                break;
+            case GameState.ready:
+                // Game을 시작하기 위한 준비 단계
+                gameState = GameState.roulette;
+                break;
+            case GameState.roulette:
+                // 우선 빙판을 깰 수 없도록
+                if (playingSettings)
+                {
+                    DisableBreaking();
+                    playingSettings = false;
+                    round++;
+                }
+                // 돌림판이 돌아가는 중의 상태
+
+                // 돌림판이 끝나면, 데드존 세팅 등등..
+                if (deadZone == null)
+                    SetDeadZone();
+                Invoke("ChangeToPlay", 3f);
+                break;
+            case GameState.play:
+                // 특수효과 등이 적용된 후 실제로 빙판을 깨는 단계
+                // 빙판을 깰 수 있도록 세팅, 미니펭귄 이동 가능, 
+                if (!playingSettings)
+                {
+                    SettingsToPlay();
+                    playingSettings = true;
+                    print("Round " + round);
+                }
+                break;
+        }
+    }
+
+    void GameRule()
+    {
+        switch(gameState)
+        {
+            case GameState.play:
+                if (deadZone.GetComponent<IceAction_KSW>().GetLife() == 0)
+                {
+                    DeadZoneKill();
+                }
+                break;
+        }
+    }
+
+    void ChangeToPlay()
+    {
+        gameState = GameState.play;
+    }
+
+    void SettingsToPlay()
+    {
+        Transform[] ices = iceGround.GetComponentsInChildren<Transform>();
+        foreach (Transform t in ices)
+        {
+            t.transform.GetComponent<IceAction_KSW>().CanBreak();
+        }
+    }
+    void DisableBreaking()
+    {
+        Transform[] ices = iceGround.GetComponentsInChildren<Transform>();
+        foreach (Transform t in ices)
+        {
+            t.transform.GetComponent<IceAction_KSW>().NotBreak();
+        }
+    }
+    
+    void SetDeadZone()
+    {
+        Transform[] ices = iceGround.GetComponentsInChildren<Transform>();
+        List<Transform> temp = new List<Transform>();
+        foreach (Transform t in ices)
+        {
+            if (t.GetComponent<IceAction_KSW>().GetLife() > 0)
+                temp.Add(t);
+        }
+        deadZone = temp[Random.Range(0, temp.Count)];
+        deadZone.GetComponent<MeshRenderer>().material = deadMat;
+    }
+
+    void DeadZoneKill()
+    {
+        Transform[] ices = iceGround.GetComponentsInChildren<Transform>();
+        foreach (Transform t in ices)
+        {
+            t.transform.GetComponent<IceAction_KSW>().LifeZero();
+        }
     }
 }
--- Assets/02_Scripts_KSW/IceAction_KSW.cs
+++ Assets/02_Scripts_KSW/IceAction_KSW.cs
@@ -7,16 +7,72 @@ public class IceAction_KSW : MonoBehaviourPun
 {
     int life;
     public Material[] mats;
+    MeshCollider mesh;
+    MeshRenderer render;
+    bool isBreakable;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        life = 3;
+        mesh = GetComponent<MeshCollider>();
+        render = GetComponent<MeshRenderer>();
+        isBreakable = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LifeCheck();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Picker") && isBreakable)
+        {
+            other.GetComponent<BoxCollider>().enabled = false;
+            StartCoroutine(CoolSet(other));
+            life--;
+            print("맞아부러써");
+        }
+    }
+    void LifeCheck()
+    {
+        if (life == 0)
+        {
+            mesh.enabled = false;
+            render.enabled = false;
+        }
+    }
+
+    public void Reset()
+    {
+        life = 3;
+        mesh.enabled = true;
+        render.enabled = true;
+    }
+
+    public void LifeZero()
+    {
+        life = 0;
+    }
+    public void NotBreak()
+    {
+        isBreakable = false;
+    }
+    public void CanBreak()
+    {
+        isBreakable = true;
+    }
+
+    public int GetLife()
+    {
+        return life;
+    }
+
+    IEnumerator CoolSet(Collider other)
+    {
+        yield return new WaitForSeconds(0.73f);
+        other.GetComponent<BoxCollider>().enabled = true;
     }
 }
